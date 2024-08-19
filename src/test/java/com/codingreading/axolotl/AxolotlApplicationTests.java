a1464138ee4d1181ab74b4f4d554efa455835e42
dayeonAhn <as14023182@gmail.com>
2024-08-19 13:25:42 +0900

    test commit

---
+++ src/test/java/com/codingreading/axolotl/AxolotlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingreading.axolotl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxolotlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
